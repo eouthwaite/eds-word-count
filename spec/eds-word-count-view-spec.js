639f6fa17e6e3bd3f8ebfacdfdd381a115feb426
Ed Outhwaite <ed.outhwaite@datavid.co.uk>
2020-10-26 18:28:58 +0000

    first commit

---
+++ spec/eds-word-count-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EdsWordCountView from '../lib/eds-word-count-view';
+
+describe('EdsWordCountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
